22d431a129cbccd29838aece8a28adb7472b08a7
Amoriss <amoriss@protonmail.com>
2024-10-06 15:48:34 -0500

    date of birth column added to database

--- BlazorAppIdentityDotNet8/BlazorAppIdentityDotNet8/Data/ApplicationUser.cs
+++ BlazorAppIdentityDotNet8/BlazorAppIdentityDotNet8/Data/ApplicationUser.cs
@@ -4,5 +4,6 @@ namespace BlazorAppIdentityDotNet8.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public DateOnly? DateOfBirth { get; set; }
 }
 
